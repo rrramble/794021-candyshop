39d4c1c4f42d245564ca9e70bea08a3591243407
Rustam <ru7ar7@gmail.com>
2018-09-27 16:58:26 +0600

    style: comply with the Linter

--- js/goods.js
+++ js/goods.js
@@ -7,9 +7,6 @@
 
 (function () {
 
-  var COMMODITY_HTML_ID_HEAD = 'commodity';
-  var TROLLEY_HTML_ID_HEAD = 'trolley-commodity';
-
   var GOODS_HTML_TEMPLATE_SELECTOR = '#card';
   var GOODS_HTML_SELECTOR = '.catalog__cards';
 
@@ -23,12 +20,12 @@
     MIN_RANGE_BTN_TEXT_SELECTOR: '.range__price--min',
     MAX_RANGE_BTN_TEXT_SELECTOR: '.range__price--max',
     RANGE_BTN_PARENT_SELECTOR: '.range__filter'
-  }
+  };
 
   var Order = {
     MAIN_SELECTOR: '.buy',
     SUBMIT_BTN_SELECTOR: '.buy__submit-btn'
-  }
+  };
 
   var PAYMENT = {
     MAIN_SELECTOR: '.payment',
@@ -42,7 +39,7 @@
     CARD_DATE_INPUT_SELECTOR: '#payment__card-date',
     CARD_CVC_INPUT_SELECTOR: '#payment__card-cvc',
     CARD_HOLDER_INPUT_SELECTOR: '#payment__cardholder'
-  }
+  };
 
   var DELIVERY = {
     MAIN_SELECTOR: '.deliver',
@@ -50,24 +47,24 @@
     SELF_TAKE_OUT_SELECTOR: '.toggle-btn__input[value="store"]',
     BY_COURIER_SELECTOR: '.toggle-btn__input[value="courier"]',
     SUBWAY_STATIONS_SELECTOR: '.deliver__stores'
-  }
+  };
 
 
-  var GOODS_IN_TROLLEY_COUNT = 3;
-
-
-/*
- * Main code
- */
+  /*
+   * Main code
+   */
 
   var catalog = new window.Catalog(window.mockGoods.get);
   var trolley = new window.Trolley(catalog);
   var dom = new window.Dom(
-    catalog, GOODS_HTML_TEMPLATE_SELECTOR, GOODS_HTML_SELECTOR,
-    trolley, TROLLEY_HTML_TEMPLATE_SELECTOR, TROLLEY_HTML_SELECTOR
+      catalog, GOODS_HTML_TEMPLATE_SELECTOR, GOODS_HTML_SELECTOR,
+      trolley, TROLLEY_HTML_TEMPLATE_SELECTOR, TROLLEY_HTML_SELECTOR
   );
+  dom.renderCatalogDom();
+  dom.renderTrolleyDom();
 
   /*
+  var GOODS_IN_TROLLEY_COUNT = 3;
   putRandomGoodsInTrolley(
       catalog, catalogDom.updateView,
       trolley, trolleyDom.updateView,
@@ -86,38 +83,38 @@
     setFieldsForCardPayment(true);
 
     window.utils.setDomHandlers(
-      document, PAYMENT.METHOD_SELECTOR,
-      paymentTypeHandlers,
-      'click'
+        document, PAYMENT.METHOD_SELECTOR,
+        paymentTypeHandlers,
+        'click'
     );
 
     window.utils.setDomHandlers(
-      document, Order.MAIN_SELECTOR,
-      checkOrderFormValidity,
-      'submit'
+        document, Order.MAIN_SELECTOR,
+        checkOrderFormValidity,
+        'submit'
     );
 
     window.utils.setDomHandlers(
-      document, PAYMENT.CARD_FIELDS_MAIN_SELECTOR,
-      checkOrderFormValidity,
-      'change'
+        document, PAYMENT.CARD_FIELDS_MAIN_SELECTOR,
+        checkOrderFormValidity,
+        'change'
     );
 
     window.utils.setDomHandlers(
-      document, DELIVERY.MAIN_SELECTOR,
-      deliveryHandlers,
-      'click'
+        document, DELIVERY.MAIN_SELECTOR,
+        deliveryHandlers,
+        'click'
     );
 
     window.utils.setDomHandlers(
-      document, Filter.MAIN_SELECTOR,
-      filterHandlers,
-      'mouseup'
+        document, Filter.MAIN_SELECTOR,
+        filterHandlers,
+        'mouseup'
     );
   }
 
-  function paymentTypeHandlers (evt) {
-    switch(true) {
+  function paymentTypeHandlers() {
+    switch (true) {
       case (window.utils.isChecked(PAYMENT.CARD_LABEL_SELECTOR)):
         adjustFormForCardPayment();
         break;
@@ -127,18 +124,18 @@
     }
   }
 
-  function adjustFormForCardPayment () {
+  function adjustFormForCardPayment() {
     window.utils.showHtmlSelector(document, PAYMENT.CARD_FORM_SELECTOR);
     setFieldsForCardPayment(true);
   }
 
-  function adjustFormForCashPayment () {
+  function adjustFormForCashPayment() {
     window.utils.hideHtmlSelector(document, PAYMENT.CARD_FORM_SELECTOR);
     setFieldsForCardPayment(false);
-   	resetCardValidity();
+    resetCardValidity();
   }
 
-  function setFieldsForCardPayment (isToBeSet) {
+  function setFieldsForCardPayment(isToBeSet) {
     window.utils.setInputToBeRequired(isToBeSet, PAYMENT.CARD_NUMBER_INPUT_SELECTOR);
     window.utils.setInputToBeRequired(isToBeSet, PAYMENT.CARD_DATE_INPUT_SELECTOR);
     window.utils.setInputToBeRequired(isToBeSet, PAYMENT.CARD_CVC_INPUT_SELECTOR);
@@ -149,7 +146,7 @@
     switch (true) {
       case (!isCardNumberValid()):
         window.utils.setDomValid(false, PAYMENT.CARD_NUMBER_INPUT_SELECTOR);
-        break;    
+        break;
       case (!isCardDateValid()):
         window.utils.setDomValid(true, PAYMENT.CARD_NUMBER_INPUT_SELECTOR);
         window.utils.setDomValid(false, PAYMENT.CARD_DATE_INPUT_SELECTOR);
@@ -166,43 +163,43 @@
         window.utils.setDomValid(false, PAYMENT.CARD_HOLDER_INPUT_SELECTOR);
         break;
       default:
-      	resetCardValidity();
+        resetCardValidity();
     }
   }
 
-  function resetCardValidity () {
+  function resetCardValidity() {
     window.utils.setDomValid(true, PAYMENT.CARD_NUMBER_INPUT_SELECTOR);
     window.utils.setDomValid(true, PAYMENT.CARD_DATE_INPUT_SELECTOR);
     window.utils.setDomValid(true, PAYMENT.CARD_CVC_INPUT_SELECTOR);
     window.utils.setDomValid(true, PAYMENT.CARD_HOLDER_INPUT_SELECTOR);
   }
 
-  function isCardNumberValid () {
+  function isCardNumberValid() {
     var cardNumber = window.utils.getDomValue(document, PAYMENT.CARD_NUMBER_INPUT_SELECTOR);
     var result = window.utils.isLuhnChecked(cardNumber);
     return result;
   }
 
-  function isCardDateValid () {
+  function isCardDateValid() {
     var cardDate = window.utils.getDomValue(document, PAYMENT.CARD_DATE_INPUT_SELECTOR);
     var result = window.utils.isCardDateChecked(cardDate);
     return result;
   }
 
-  function isCardCvcValid () {
+  function isCardCvcValid() {
     var cvc = window.utils.getDomValue(document, PAYMENT.CARD_CVC_INPUT_SELECTOR);
     var state = window.utils.isCvcChecked(cvc);
     return state;
   }
 
-  function isCardholderNameValid () {
+  function isCardholderNameValid() {
     var cardholder = window.utils.getDomValue(document, PAYMENT.CARD_CVC_INPUT_SELECTOR);
     var state = window.utils.isCacrdholderNameChecked(cardholder);
     return state;
   }
 
-  function deliveryHandlers (evt) {
-    switch(true) {
+  function deliveryHandlers() {
+    switch (true) {
       case (window.utils.isChecked(DELIVERY.BY_COURIER_SELECTOR)):
         window.utils.hideHtmlSelector(document, DELIVERY.SUBWAY_STATIONS_SELECTOR);
         break;
@@ -213,8 +210,8 @@
     }
   }
 
-  function filterHandlers (evt) {
-    switch(true) {
+  function filterHandlers(evt) {
+    switch (true){
       case (evt.target.classList.contains(Filter.RANGE_MIN_BTN_CLASS)):
         updateSliderPositionValue();
         break;
@@ -225,18 +222,19 @@
     }
   }
 
-  function updateSliderPositionValue () {
+  function updateSliderPositionValue() {
     var minSliderValue = getMinSliderValue();
     var maxSliderValue = getMaxSliderValue();
     window.utils.setDomTextContent(document, Filter.MIN_RANGE_BTN_TEXT_SELECTOR, minSliderValue);
     window.utils.setDomTextContent(document, Filter.MAX_RANGE_BTN_TEXT_SELECTOR, maxSliderValue);
 
-    function getMinSliderValue () {
+    function getMinSliderValue() {
       var parentWidth = window.utils.getHtmlSelectorWidth(Filter.RANGE_BTN_PARENT_SELECTOR);
       var width = window.utils.getHtmlClassLeftProperty(Filter.RANGE_MIN_BTN_CLASS);
       return window.utils.intPercent(parentWidth, width);
     }
-    function getMaxSliderValue () {
+
+    function getMaxSliderValue() {
       var parentWidth = window.utils.getHtmlSelectorWidth(Filter.RANGE_BTN_PARENT_SELECTOR);
       var width = window.utils.getHtmlClassRightProperty(Filter.RANGE_MAX_BTN_CLASS);
       return window.utils.intPercent(parentWidth, parentWidth - width);
