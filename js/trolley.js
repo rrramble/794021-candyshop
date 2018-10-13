a74c2f69852eafc195d4810ad8464437b3c66503
Rustam Arkharov <ru7ar7@gmail.com>
2018-10-14 01:30:28 +0600

    refactor: turn to 'function declaration' from 'function expression' due to #D5 criterion

--- js/trolley.js
+++ js/trolley.js
@@ -2,74 +2,26 @@
 
 
 /*
- * Trolley with goods
+ * Trolley of goods
  */
 
 (function () {
 
   window.Trolley = function (catalog) {
-
-    this.getGoods = function () {
-      return this.goods;
-    };
-
-    this.getItem = function (id) {
-      return this.getGoods()[id];
-    };
-
-    this.getAmount = function (id) {
-      return this.getItem(id).amount;
-    };
-
-    this.getCount = function () {
-      var result = this.getGoods().reduce(function (accu, item) {
-        return item.amount > 0 ? ++accu : accu;
-      }, 0);
-      return result.toFixed(0);
-    };
-
-    this.getTotalOrderedSum = function () {
-      var result = this.getGoods().reduce(function (accu, item) {
-        accu += item.amount * item.price;
-        return accu;
-      }, 0);
-      return result.toFixed(0);
-    };
-
-    this.setFavoriteStatus = function (id, favoriteStatus) {
-      this.getItem(id).favorite = favoriteStatus;
-      return favoriteStatus;
-    };
-
-    this.putItem = function (id, amount) {
-      var actualAmount = amount;
-      if (!actualAmount) {
-        actualAmount = 1;
-      }
-      this.getItem(id).amount += actualAmount;
-      return actualAmount;
-    };
-
-    this.takeItem = function (id, amount) {
-      if (this.getItem(id).amount < amount) {
-        return 0;
-      }
-      this.getItem(id).amount -= amount;
-      return amount;
-    };
-
-    this.isEmpty = function () {
-      return this.goods.every(function (item) {
-        return item.amount <= 0;
-      });
-    };
-
-    // Constructor
+    this.getGoods = getGoods;
+    this.getItem = getItem;
+    this.getAmount = getAmount;
+    this.getCount = getCount;
+    this.getTotalOrderedSum = getTotalOrderedSum;
+    this.setFavoriteStatus = setFavoriteStatus;
+    this.putItem = putItem;
+    this.takeItem = takeItem;
+    this.isEmpty = isEmpty;
 
     this.goods = catalog.getGoods().map(fillItem);
+    return;
 
-
-    // Miscelaneous functions
+    // End of constructor
 
     function fillItem(commodityItem) {
       var item = Object.assign({}, commodityItem);
@@ -77,5 +29,60 @@
       return item;
     }
 
+    function getGoods() {
+      return this.goods;
+    }
+
+    function getItem(id) {
+      return this.getGoods()[id];
+    }
+
+    function getAmount(id) {
+      return this.getItem(id).amount;
+    }
+
+    function getCount() {
+      var result = this.getGoods().reduce(function (accu, item) {
+        return item.amount > 0 ? ++accu : accu;
+      }, 0);
+      return result.toFixed(0);
+    }
+
+    function getTotalOrderedSum() {
+      var result = this.getGoods().reduce(function (accu, item) {
+        accu += item.amount * item.price;
+        return accu;
+      }, 0);
+      return result.toFixed(0);
+    }
+
+    function setFavoriteStatus(id, favoriteStatus) {
+      this.getItem(id).favorite = favoriteStatus;
+      return favoriteStatus;
+    }
+
+    function putItem(id, amount) {
+      var actualAmount = amount;
+      if (!actualAmount) {
+        actualAmount = 1;
+      }
+      this.getItem(id).amount += actualAmount;
+      return actualAmount;
+    }
+
+    function takeItem(id, amount) {
+      if (this.getItem(id).amount < amount) {
+        return 0;
+      }
+      this.getItem(id).amount -= amount;
+      return amount;
+    }
+
+    function isEmpty() {
+      return this.goods.every(function (item) {
+        return item.amount <= 0;
+      });
+    }
   };
+
 })();
